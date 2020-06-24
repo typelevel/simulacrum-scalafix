845a44ca682d4449eb8820cc27473c6406e0cc4f
Travis Brown <travisrobertbrown@gmail.com>
2020-06-24 04:24:38 -0500

    Add @compileTimeOnly

--- annotation/src/main/scala/org/typelevel/simulacrum/package.scala
+++ annotation/src/main/scala/org/typelevel/simulacrum/package.scala
@@ -1,7 +1,12 @@
 package simulacrum
 
-import scala.annotation.Annotation
+import scala.annotation.{Annotation, compileTimeOnly}
 
+@compileTimeOnly("typeclass annotation should not be used after Scalafix rule application")
 final class typeclass(excludeParents: List[String] = Nil, generateAllOps: Boolean = true) extends Annotation
+
+@compileTimeOnly("op annotation should not be used after Scalafix rule application")
 final class op(name: String, alias: Boolean = false) extends Annotation
+
+@compileTimeOnly("noop annotation should not be used after Scalafix rule application")
 final class noop() extends Annotation
